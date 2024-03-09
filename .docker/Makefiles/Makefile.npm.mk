4ea59033cbc346def1e39f4994c90f30dcada942
aymericcucherousset <aymeric.cucherousset@gmail.com>
2024-03-09 12:44:10 +0100

    feat: add node with npm in docker #30

---
+++ .docker/Makefiles/Makefile.npm.mk
@@ -0,0 +1,18 @@
+NPM = $(EXEC) npm
+
+## —— 📦 NPM 📦 ——
+
+npm-install: ## Install npm dependencies
+	$(NPM) install $(filter-out $@,$(MAKECMDGOALS))
+
+npm-update: ## Update npm dependencies
+	$(NPM) update
+
+npm-run: ## Run npm script
+	$(NPM) run $(filter-out $@,$(MAKECMDGOALS))
+
+npm-build: ## Build npm assets
+	$(NPM) run build
+
+npm-watch: ## Watch npm assets
+	$(NPM) run watch
